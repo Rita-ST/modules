92b52a245463fb50569fc9644930dff29acac431
Rita-36173 <londiwegadlela44@gmail.com>
2026-05-07 07:34:42 +0000

    get

--- eswatini_news.py
+++ eswatini_news.py
@@ -1,1 +1,3 @@
-
+import requests 
+x=requests.get("https://www.times.co.sz/news/readmore.php?bhsadjgfoh=Ezulwini+Palazzo+hosts+1st+conference%2C+over+500+delegates&yiphi=3785&bvhdgsj=News")
+print(x.text)
